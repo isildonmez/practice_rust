c764d4691bdddb7a8311704275e3322298fcfa98
isildonmez <isil.donmez@gmail.com>
2023-08-01 15:13:23 +0800

    Add chrismas_carol lyrics

---
+++ christmas_carol/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let chorus =
+    "Come and worship, come and worship\n\
+    Worship Christ the newborn King\n";
+
+    let part1 =
+    "Angels from the realms of glory\n\
+    Wing your flight over all the earth\n\
+    Ye, who sang creations story\n\
+    Now proclaim Messiah's birth\n";
+
+    let part2 =
+    "Shepherds in the fields abiding\n\
+    Watching over your flocks by night\n\
+    God with man is now residing\n\
+    Yonder shines the Infant light\n";
+
+    let and_so_on = "...";
+
+
+    println!("{part1}{chorus}{part2}{chorus}{and_so_on}");
+}
